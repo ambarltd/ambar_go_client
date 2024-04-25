845f957d003c981109fa2f2f1b9ac0b6e5fdf2ce
Tom Schutte <tom@ambar.cloud>
2024-04-25 17:12:39 -0500

    Fix imports

--- test/api_ambar_test.go
+++ test/api_ambar_test.go
@@ -11,10 +11,10 @@ package Ambar
 
 import (
 	"context"
+	openapiclient "github.com/ambarltd/ambar_go_client"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
-	openapiclient "github.com/GIT_USER_ID/GIT_REPO_ID"
 )
 
 func Test_Ambar_AmbarAPIService(t *testing.T) {
@@ -24,7 +24,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService CreateDataDestination", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.CreateDataDestination(context.Background()).Execute()
 
@@ -36,7 +36,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService CreateDataSource", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.CreateDataSource(context.Background()).Execute()
 
@@ -48,7 +48,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService CreateFilter", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.CreateFilter(context.Background()).Execute()
 
@@ -60,7 +60,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService DeleteDataDestination", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.DeleteDataDestination(context.Background()).Execute()
 
@@ -72,7 +72,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService DeleteDataSource", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.DeleteDataSource(context.Background()).Execute()
 
@@ -84,7 +84,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService DeleteFilter", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.DeleteFilter(context.Background()).Execute()
 
@@ -96,7 +96,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService DescribeDataDestination", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.DescribeDataDestination(context.Background()).Execute()
 
@@ -108,7 +108,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService DescribeDataSource", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.DescribeDataSource(context.Background()).Execute()
 
@@ -120,7 +120,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService DescribeFilter", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.DescribeFilter(context.Background()).Execute()
 
@@ -132,7 +132,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService ListResources", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.ListResources(context.Background()).Execute()
 
@@ -144,7 +144,7 @@ func Test_Ambar_AmbarAPIService(t *testing.T) {
 
 	t.Run("Test AmbarAPIService UpdateDataDestinationCredentials", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.AmbarAPI.UpdateDataDestinationCredentials(context.Background()).Execute()
 
